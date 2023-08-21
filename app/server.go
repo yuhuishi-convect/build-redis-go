f6ceb540f0a9004038a1843ff96036b3190d2581
Yuhui Shi <yuhui@convect.ai>
2023-08-20 22:46:49 -0700

    Concurrent connection handling by goroutines

--- app/server.go
+++ app/server.go
@@ -7,6 +7,29 @@ import (
 	"os"
 )
 
+// handleRequest handles requests from clients.
+func handleRequest(conn net.Conn) {
+	defer conn.Close()
+	// Make a buffer to hold incoming data.
+	buf := make([]byte, 1024)
+	// Read each message until newline.
+	reqLen, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading: ", err.Error())
+		return
+	}
+
+	// Print the incoming message.
+	fmt.Println("Received ", string(buf[:reqLen]))
+
+	// For each message, send a pong message back to the client.
+	msg := "+PONG\r\n"
+	_, err = conn.Write([]byte(msg))
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
+	}
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -20,34 +43,16 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Accept a connection.
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-
-	// Respond to multiple messages from the client.
+	// Use goroutines to handle multiple concurrent connections from clients.
 	for {
-		// Make a buffer to hold incoming data.
-		buf := make([]byte, 1024)
-		// Read each message until newline.
-		reqLen, err := conn.Read(buf)
+		// Wait for a connection.
+		conn, err := l.Accept()
 		if err != nil {
-			fmt.Println("Error reading: ", err.Error())
-			break
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
 		}
 
-		// Print the incoming message.
-		fmt.Println("Received ", string(buf[:reqLen]))
-
-		// For each message, send a pong message back to the client.
-		msg := "+PONG\r\n"
-		_, err = conn.Write([]byte(msg))
-		if err != nil {
-			fmt.Println("Error writing to connection: ", err.Error())
-		}
+		// Handle the connection in a new goroutine.
+		go handleRequest(conn)
 	}
-
-	conn.Close()
 }

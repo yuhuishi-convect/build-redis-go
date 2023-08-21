bba6872acbd67495b917589193d2118ec188ae66
Yuhui Shi <yuhui@convect.ai>
2023-08-20 22:38:29 -0700

    Multiple message reply

--- app/server.go
+++ app/server.go
@@ -16,19 +16,38 @@ func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
+		fmt.Println("Error: ", err.Error())
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
 
+	// Accept a connection.
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	// send a RESP simple string message "+OK\r\n" to the client whenever a client pings the server
-	msg := "+PONG\r\n"
-	_, err = conn.Write([]byte(msg))
-	if err != nil {
-		fmt.Println("Error writing to connection: ", err.Error())
+
+	// Respond to multiple messages from the client.
+	for {
+		// Make a buffer to hold incoming data.
+		buf := make([]byte, 1024)
+		// Read each message until newline.
+		reqLen, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading: ", err.Error())
+			break
+		}
+
+		// Print the incoming message.
+		fmt.Println("Received ", string(buf[:reqLen]))
+
+		// For each message, send a pong message back to the client.
+		msg := "+PONG\r\n"
+		_, err = conn.Write([]byte(msg))
+		if err != nil {
+			fmt.Println("Error writing to connection: ", err.Error())
+		}
 	}
 
+	conn.Close()
 }
